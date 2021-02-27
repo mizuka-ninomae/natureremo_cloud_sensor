9bf6d46c795c51bcaaa74c67365350c32a2ddf4c
一 瑞香（Mizuka Ninomae） <74567919+mizuka-ninomae@users.noreply.github.com>
2021-02-27 16:50:14 +0900

    Update test_driver.js

--- test_driver.js
+++ test_driver.js
@@ -1,5 +1,5 @@
 const NatureRemoSensor = require('natureremo_sensor_cloud');
-let   access_token     = "gagLNUEyEgNKPgvsyr0PvZ3vZLGgmhsiSzZprnjrHPY.6YA1fdN5oVsNPMETkQNsnjx5yTHc0yj9h2148T5sdIk";
+let   access_token     = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
 
 let remo_sensor = new NatureRemoSensor(access_token, {mo: false}, function (error, value, stderr) {
   console.log (value);
