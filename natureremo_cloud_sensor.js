40f09db91f32864c7af725232993bf30eb52cece
一 瑞香（Mizuka Ninomae） <74567919+mizuka-ninomae@users.noreply.github.com>
2021-02-27 22:37:04 +0900

    Update natureremo_cloud_sensor.js

--- natureremo_cloud_sensor.js
+++ natureremo_cloud_sensor.js
@@ -3,9 +3,9 @@ let   url  = `"https://api.nature.global/1/devices"`;
 
 class NatureRemoSensor {
   constructor (access_token, {te = true, hu = true, li = true, mo = true}, callback) {
-    let   cmd  = `curl -X GET ${url} -H "accept":"application/json" -k --header "Authorization":"Bearer ${access_token}"`;
+    let   cmd    = `curl -X GET ${url} -H "accept":"application/json" -k --header "Authorization":"Bearer ${access_token}"`;
     exec (cmd, function (error, stdout, stderr) {
-      let json          = JSON.parse(stdout);
+      let json   = JSON.parse(stdout);
       let te_val = te ? json[0].newest_events.te.val : null;
       let hu_val = hu ? json[0].newest_events.hu.val : null;
       let li_val = li ? json[0].newest_events.il.val : null;
