54e9bd3a02e588a18a739f7ef9b46757ef8da3b0
mizuka-ninomae <mizuka_ninomae@icloud.com>
2021-02-28 19:09:05 +0900

    更新

--- natureremo_cloud_sensor.js
+++ natureremo_cloud_sensor.js
@@ -1,23 +1,23 @@
-const exec = require("child_process").exec;
+const exec = require ("child_process").exec;
 let   url  = `"https://api.nature.global/1/devices"`;
 
 class NatureRemoSensor {
-  constructor (access_token, {te = true, hu = true, li = true, mo = true}, callback) {
-    let   cmd    = `curl -X GET ${url} -H "accept":"application/json" -k --header "Authorization":"Bearer ${access_token}"`;
+  constructor (access_token, callback) {
+    let cmd = `curl -X GET ${url} -H "accept":"application/json" -k --header "Authorization":"Bearer ${access_token}"`;
     exec (cmd, function (error, stdout, stderr) {
-      let json   = JSON.parse(stdout);
-      let te_val = te ? json[0].newest_events.te.val : null;
-      let hu_val = hu ? json[0].newest_events.hu.val : null;
-      let li_val = li ? json[0].newest_events.il.val : null;
-      let mo_val = mo ? json[0].newest_events.mo.val : null;
+      let json   = JSON.parse (stdout);
+      let te_val = json[0].newest_events.te.val;
+      let hu_val = json[0].newest_events.hu.val;
+      let li_val = json[0].newest_events.il.val;
+      let mo_val = json[0].newest_events.mo.val;
 
-  	  callback (error, JSON.stringify({ te: te_val, hu: hu_val, il: li_val, mo: mo_val}), stderr)
+  	  callback (error, JSON.stringify({te: te_val, hu: hu_val, il: li_val, mo: mo_val}), stderr)
     })
   }
 }
 
 if (require.main === module) {
-  new NatureRemoSensor (process.argv[2], process.argv[3], function(error, value, stderr){
+  new NatureRemoSensor (process.argv[2], function(error, value, stderr){
     console.log (value);
     console.log (error);
     console.log (stderr);
--- test_driver.js
+++ test_driver.js
@@ -1,7 +1,7 @@
 const NatureRemoSensor = require('natureremo_cloud_sensor');
-let   access_token     = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+let   access_token     = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
 
-let remo_sensor = new NatureRemoSensor(access_token, {mo: false}, function (error, value, stderr) {
+let remo_sensor = new NatureRemoSensor(access_token, function (error, value, stderr) {
   console.log (value);
   console.log (error);
   console.log (stderr);
