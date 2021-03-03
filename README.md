2821010e1f189a1fb2a60e38f4cc2befedb7371e
mizuka-ninomae <mizuka_ninomae@icloud.com>
2021-03-03 11:02:25 +0900

    update

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
+<p align="center">
+  <img src="img/nature_remo.png" width="300">
+</p>
 
+## 概要（Overview）
+
+Nature Remoシリーズの温度計、湿度計、照度計、モーションセンサーで各値を測定するプログラムです。（This is a program for measuring each value with a Nature Remo series thermometer, hygrometer, Illuminance meter, and motion sensor.）
+
+`node natureremo_cloud_sensor <access_token(FirstArgument[Required]) device_name(SecondArgument[Optional])>`
+
+### 特徴・要件
+
+* `carl` が必要です。（`carl` required）
+* モジュールですが単体での起動が可能です。（Although it is a module, it can be started by itself.）
+
+### テスト環境（Testing Environment）
+
+* Raspberry Pi 4 modelB 4GB(Raspbian GNU/Linux 10 (buster)) + Nature Remo +Nature Remo mini
+* Windows10 + Nature Remo +Nature Remo mini
+* MacOS BigSur (11.1) + Nature Remo +Nature Remo mini
+（Untested：Nature Remo 2, Nature Remo 3, Nature Remo mini 2）
+
+ここに書いてある事はわたしがテストした環境であり、動作を保証するものではありません。（What is written here is the environment I tested, and I do not guarantee the operation.）
+もしかしたら他の環境でも動くかもしれません。
--- natureremo_cloud_sensor.js
+++ natureremo_cloud_sensor.js
@@ -1,23 +1,50 @@
 const exec = require ("child_process").exec;
 let   url  = `"https://api.nature.global/1/devices"`;
+let   num  = null;
+let   te_val, hu_val, li_val, mo_val;
 
 class NatureRemoSensor {
-  constructor (access_token, callback) {
+  constructor (access_token, dev_name, callback) {
     let cmd = `curl -X GET ${url} -H "accept":"application/json" -k --header "Authorization":"Bearer ${access_token}"`;
     exec (cmd, function (error, stdout, stderr) {
-      let json   = JSON.parse (stdout);
-      let te_val = json[0].newest_events.te.val;
-      let hu_val = json[0].newest_events.hu.val;
-      let li_val = json[0].newest_events.il.val;
-      let mo_val = json[0].newest_events.mo.val;
-
-  	  callback (error, JSON.stringify({te: te_val, hu: hu_val, il: li_val, mo: mo_val}), stderr)
+      let obj   = JSON.parse (stdout);
+      if (dev_name == null) {
+        num = 0;
+      }
+      else {
+        for (let i in obj) {
+          if (dev_name == obj[i].name) {
+            num = i;
+            break;
+          }
+        }
+      }
+      if (num == null) {
+        throw new Error('Device Name not found')
+      }
+      else {
+        if (obj[num].firmware_version.startsWith("Remo-mini")) {
+          te_val = obj[num].newest_events.te.val;
+          hu_val = null;
+          li_val = null;
+          mo_val = null;
+        }
+        else {
+          te_val = obj[num].newest_events.te.val;
+          hu_val = obj[num].newest_events.hu.val;
+          li_val = obj[num].newest_events.il.val;
+          mo_val = obj[num].newest_events.mo.val;
+        }
+        let value = {te: te_val, hu: hu_val, li: li_val, mo: mo_val};
+        callback (error, value, stderr)
+        return;
+      }
     })
   }
 }
 
 if (require.main === module) {
-  new NatureRemoSensor (process.argv[2], function(error, value, stderr){
+  new NatureRemoSensor (process.argv[2], process.argv[3], function(error, value, stderr) {
     console.log (value);
     console.log (error);
     console.log (stderr);
